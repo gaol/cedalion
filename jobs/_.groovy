aef987d5f02c443cbc6582b7803be652b09dbf8e
Lin Gao <lgao@redhat.com>
2022-03-31 22:03:26 +0800

    generate EPA 7.4 jobs

--- jobs/_.groovy
+++ jobs/_.groovy
@@ -3,3 +3,7 @@ new eap7.Builder(branch:'main',
                  gitRepositoryUrl: "git@github.com:wildfly/wildfly.git"
                 ).buildAndTest(this)
 EapView.jobList(this, 'wildfly', 'wildfly.*')
+
+new eap7.Builder(branch:'7.4.x').buildAndTest(this)
+
+EapView.jobList(this, 'eap7', 'eap-.*')
